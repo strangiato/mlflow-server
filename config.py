76c72f0382c51851c3113fce36a4adc76d35caeb
strangiato <trevorroyer@gmail.com>
2021-01-22 23:55:39 -0600

    update descriptions

--- config.py
+++ config.py
@@ -7,9 +7,10 @@ class MlflowConfig:
     host_ip = environ.var("0.0.0.0", help="The host IP address used to start MLFlow Server")
     host_port = environ.var("8080", help="The host port used to start MLFlow Server")
 
-    s3_endpoint_url = environ.var(help="The ")
-    access_key = environ.var(name="AWS_ACCESS_KEY_ID")
-    secret_key = environ.var(name="AWS_SECRET_KEY_ID")
+    s3_endpoint_url = environ.var(help="The S3 endpoint URL that MLFlow Server will store artifacts")
+    s3_bucket = environ.var("mlflow", help="The bucket in the S3 instance used by MLFlow Server to store artifacts")
+    access_key = environ.var(name="AWS_ACCESS_KEY_ID", help="The Access Key for the S3 instance")
+    secret_key = environ.var(name="AWS_SECRET_KEY_ID", help="The Secret Key for the S3 instance")
 
     @environ.config
     class DB:

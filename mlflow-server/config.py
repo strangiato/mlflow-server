ffe2532b1ff6410a50fc693d42afa3067b97405d
Trevor Royer <troyer@redhat.com>
2022-04-18 15:03:13 -0500

    Setup application configuration and start

---
+++ mlflow-server/config.py
@@ -0,0 +1,63 @@
+"""Configuration file for managing environment variables."""
+
+import environ
+
+
+@environ.config()
+class AppConfig:
+    """Application configuration object used for managing environment variables."""
+
+    @environ.config()
+    class MLFlow:
+        """Configuration object for the mlflow options."""
+
+        host = environ.var(
+            default="0.0.0.0", help="The host address for MLFlow server to start."
+        )
+
+        port = environ.var(default="8080", help="The port for MLFlow server to start.")
+
+    mlflow = environ.group(MLFlow)
+
+    @environ.config()
+    class Database:
+        """Configuration object for the database."""
+
+        url = environ.var(help="The url for the database")
+
+        host = environ.var(help="The hostname of the database")
+
+        username = environ.var(help="The username for the database")
+
+        password = environ.var(help="The password for the database")
+
+    database = environ.group(Database)
+
+    @environ.config()
+    class S3:
+        """App configuration object used for managing s3 objects."""
+
+        endpoint_url = environ.var(
+            name="MLFLOW_S3_ENDPOINT_URL",
+            help="The S3 endpoint url that MLFlow will utilize for artifact storage",
+        )
+
+        bucket_name = environ.var(
+            name="MLFLOW_S3_BUCKET_NAME",
+            help="The name of the S3 bucket that MLFlow will utilize for artifact storage",
+        )
+
+        access_key = environ.var(
+            name="AWS_ACCESS_KEY_ID",
+            help="The Access Key ID for the S3 source bucket instance",
+        )
+        secret_key = environ.var(
+            name="AWS_SECRET_ACCESS_KEY",
+            help="The Secret Access Key for the S3 source bucket instance",
+        )
+
+    s3 = environ.group(S3)
+
+
+if __name__ == "__main__":
+    print(environ.generate_help(AppConfig, display_defaults=True))
